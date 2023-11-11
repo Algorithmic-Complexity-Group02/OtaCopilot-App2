2b667906888ec603e14172806a137c31f2e998f4
RamiroVT <u202123714@upc.edu.pe>
2023-11-11 02:32:11 -0500

    feat: add anime model

---
+++ src/app/models/anime.ts
@@ -0,0 +1,14 @@
+export interface Anime {
+    uid: number;
+    title: string;
+    synopsis: string;
+    genre: string[];
+    aired: string;
+    episodes: number;
+    members: number;
+    popularity: number;
+    ranked: number;
+    score: number;
+    img_url: string;
+    link: string
+}
